966e302889e5fea5a804ec6285706c824fffda5a
cokkum113 <codingwhizkid@naver.com>
2022-03-15 00:29:09 +0900

    스티커 dp 문제

--- 9465.py
+++ 9465.py
@@ -10,15 +10,19 @@ for _ in range(t):
     for _ in range(2):
         arr.append(list(map(int, input().split())))
 
+    if n == 1:
+        dp[0][0] = arr[0][0]
+        dp[1][0] = arr[1][0]
     
-    dp[0][0] = arr[0][0]
-    dp[1][0] = arr[1][0]
+    else:
+        dp[0][0] = arr[0][0]
+        dp[1][0] = arr[1][0]
 
-    dp[0][1] = arr[1][0] + arr[0][1]
-    dp[1][1] = arr[0][0] + arr[1][1]
+        dp[0][1] = arr[1][0] + arr[0][1]
+        dp[1][1] = arr[0][0] + arr[1][1]
 
-    for i in range(2, n):
-        dp[0][i] = max(dp[1][i - 1] + arr[0][i], dp[1][i - 2] + arr[0][i])
-        dp[1][i] = max(dp[0][i - 1] + arr[1][i], dp[0][i - 2] + arr[1][i])
+        for i in range(2, n):
+            dp[0][i] = max(dp[1][i - 1] + arr[0][i], dp[1][i - 2] + arr[0][i])
+            dp[1][i] = max(dp[0][i - 1] + arr[1][i], dp[0][i - 2] + arr[1][i])
 
     print(max(dp[1][n - 1], dp[0][n - 1]))
