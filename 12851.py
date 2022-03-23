be073dca64d2620a3a162f0bea691ec277e6873a
cokkum113 <codingwhizkid@naver.com>
2022-03-23 22:12:47 +0900

    12851

--- 12851.py
+++ 12851.py
@@ -7,24 +7,24 @@ n, k = map(int, input().split())
 visited = [[-1, 0] for _ in range(100001)]
 
 def bfs(st):
-    que = deque()
-    que.append(st)
+    q = deque()
+    q.append(st)
     visited[st][0] = 0
     visited[st][1] = 1
 
-    while que:
-        pos = que.popleft()
-
-        for i in [pos - 1, pos + 1, pos * 2]:
-            if 0 <= i <= 100000:
+    while q:
+        pos = q.popleft()
+        for i in [pos + 1, pos -1, pos * 2]:
+            if 0 <= i < 100001:
                 if visited[i][0] == -1:
                     visited[i][0] = visited[pos][0] + 1
                     visited[i][1] = visited[pos][1]
-                    que.append(i)
-            
+                    q.append(i)
+                
                 elif visited[i][0] == visited[pos][0] + 1:
-                    visited[i][1] += visited[pos][1] 
-
+                    visited[i][1] += visited[pos][1]
+                
 bfs(n)
 print(visited[k][0])
 print(visited[k][1])
+
