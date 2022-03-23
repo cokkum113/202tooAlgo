a84506db8a1e27bb214a520976b6c223217aa859
cokkum113 <codingwhizkid@naver.com>
2022-03-23 21:10:11 +0900

    9465

--- 9465.py
+++ 9465.py
@@ -9,15 +9,21 @@ for _ in range(t):
     graph = []
     for _ in range(2):
         graph.append(list(map(int, input().split())))
+    
+    if n == 1:
+        dp[0][0] = graph[0][0]
+        dp[1][0] = graph[1][0]
+    
+    else:
+        dp[0][0] = graph[0][0]
+        dp[1][0] = graph[1][0]
 
-    dp[0][0] = graph[0][0]
-    dp[1][0] = graph[1][0]
+        dp[0][1] = graph[0][1] + dp[1][0]
+        dp[1][1] = graph[1][1] + dp[0][0]
 
-    dp[0][1] = graph[0][1] + dp[1][0]
-    dp[1][1] = graph[1][1] + dp[0][0]
 
-    for i in range(2, n):
-        dp[0][i] = graph[0][i] + max(dp[1][i - 1], dp[1][i - 2])
-        dp[1][i] = graph[1][i] + max(dp[0][i - 1], dp[0][i - 2])
+        for i in range(2, n):
+            dp[0][i] = graph[0][i] + max(dp[1][i - 1], dp[1][i - 2])
+            dp[1][i] = graph[1][i] + max(dp[0][i - 1], dp[0][i - 2])
     
     print(max(dp[0][n - 1],dp[1][n - 1]))
