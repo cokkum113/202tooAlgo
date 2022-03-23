7d42e4041f0e61f9470a0f360f1faa81125c67b9
cokkum113 <codingwhizkid@naver.com>
2022-03-23 21:05:15 +0900

    9465_2

--- 9465.py
+++ 9465.py
@@ -2,27 +2,22 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
-
 for _ in range(t):
     n = int(input())
     dp = [[0] * n for _ in range(2)]
-    arr = []
+
+    graph = []
     for _ in range(2):
-        arr.append(list(map(int, input().split())))
+        graph.append(list(map(int, input().split())))
 
-    if n == 1:
-        dp[0][0] = arr[0][0]
-        dp[1][0] = arr[1][0]
+    dp[0][0] = graph[0][0]
+    dp[1][0] = graph[1][0]
+
+    dp[0][1] = graph[0][1] + dp[1][0]
+    dp[1][1] = graph[1][1] + dp[0][0]
+
+    for i in range(2, n):
+        dp[0][i] = graph[0][i] + max(dp[1][i - 1], dp[1][i - 2])
+        dp[1][i] = graph[1][i] + max(dp[0][i - 1], dp[0][i - 2])
     
-    else:
-        dp[0][0] = arr[0][0]
-        dp[1][0] = arr[1][0]
-
-        dp[0][1] = arr[1][0] + arr[0][1]
-        dp[1][1] = arr[0][0] + arr[1][1]
-
-        for i in range(2, n):
-            dp[0][i] = max(dp[1][i - 1] + arr[0][i], dp[1][i - 2] + arr[0][i])
-            dp[1][i] = max(dp[0][i - 1] + arr[1][i], dp[0][i - 2] + arr[1][i])
-
-    print(max(dp[1][n - 1], dp[0][n - 1]))
+    print(max(dp[0][n - 1],dp[1][n - 1]))
