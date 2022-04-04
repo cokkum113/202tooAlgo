fff276020c7148473fd2beec0d4677661706c472
cokkum113 <codingwhizkid@naver.com>
2022-04-05 01:08:54 +0900

    그리디

--- greedy.py
+++ greedy.py
@@ -29,3 +29,41 @@ def solution(n, lost, reserve):
     
     answer += len(aa)    
     return answer
+
+#탐욕법
+def solution(name):
+    
+    cnt  = 0
+    mini = len(name) - 1
+    
+    for i, val in enumerate(name):
+        cnt += min(ord(val) - ord('A') , ord('Z') - ord(val) + 1)
+        
+        j = i + 1
+        while j < len(name) and name[j] == 'A':
+            j += 1
+        
+        mini = min(mini, 2 * i + len(name) - j, i + 2 * (len(name) - j))
+    
+    cnt += mini
+    return cnt
+
+
+#큰 수 만들기
+from itertools import combinations
+def solution(number, k):
+    answer = ''
+    a = []
+    for i in number:
+        while len(a) != 0 and a[-1] < i:
+            if k > 0:
+                a.pop()
+                k -= 1
+            else:
+                break
+        a.append(i)
+    if k > 0:
+        a = a[:-k]
+    answer = ''.join(a)
+    
+    return answer
