beac349251fcfc31758c557c5224753370ed00d2
cokkum113 <codingwhizkid@naver.com>
2022-04-28 12:51:58 +0900

    15663

--- 15663.py
+++ 15663.py
@@ -9,27 +9,20 @@ nums.sort()
 visited = [False] * len(nums)
 
 ans = []
-aa = []
-cc = []
 def backtracking(index):
-    ############ans를 리턴할때 값이 안사라지게 하려면 deepcopy말고 뭘 써야할지 
     if len(ans) == m:
-        aa = deepcopy(ans)
-        if aa not in cc:
-            cc.append(aa)
+        print(' '.join(map(str,ans)))
         return
-        
-    for i in range(1, len(nums) + 1):
-        if visited[i - 1] == False:
-            ans.append(nums[i - 1])
-            visited[i - 1] = True
+    aa = 0
+    for i in range(n):
+        if visited[i] == False and aa != nums[i]:
+            ans.append(nums[i])
+            visited[i] = True
+            aa = nums[i]
             backtracking(index + 1)
             ans.pop()
-            visited[i - 1] = False
+            visited[i] = False
         else:
             continue
 
 backtracking(0)
-
-for i in cc:
-    print(*i)
