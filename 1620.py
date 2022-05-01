7345285ca569195ff548090de26cd064ee23f2e8
cokkum113 <codingwhizkid@naver.com>
2022-05-01 13:03:04 +0900

    1620

---
+++ 1620.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m =  map(int, input().split())
+
+names = dict()
+indexing = dict()
+
+for i in range(1, n + 1):
+    s = input().rstrip()
+    names[i] = s
+    indexing[s] = i
+
+for i in range(m):
+    s = input().rstrip()
+    if ord('A') <= ord(s[0]) <= ord('Z'):
+        print(indexing[s])
+    else:
+        print(names[int(s)])
+
+
+    
