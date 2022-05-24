25b7bf030ceec0b4142492bfa3ea50b12c44c6bd
cokkum113 <codingwhizkid@naver.com>
2022-05-24 15:24:46 +0900

    again

--- 2589.py
+++ 2589.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-graph = []
 n, m = map(int, input().split())
+#최단거리는 미니멈값이 되면돼
+
+graph = []
 for _ in range(n):
     s = input().rstrip()
-    r = []
-    for i in s:
-        r.append(i)
-    graph.append(r)
+    ll = []
+    for i in s: 
+        ll.append(i)
+    graph.append(ll)
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
@@ -19,26 +21,28 @@ def bfs(x, y):
     que = deque()
     que.append([x, y])
     visited[x][y] = True
+
     while que:
         x1, y1 = que.popleft()
         for d in range(4):
             nx = x1 + dx[d]
             ny = y1 + dy[d]
-
             if 0 <= nx < n and 0 <= ny < m:
                 if visited[nx][ny] == False and graph[nx][ny] != 'W':
-                    que.append([nx,ny])
                     graph[nx][ny] = graph[x1][y1] + 1
                     cnt = max(cnt, graph[nx][ny])
                     visited[nx][ny] = True
+                    que.append([nx, ny])
     return cnt
 
-c = 0
+ans = 0
 for i in range(n):
     for j in range(m):
         if graph[i][j] != 'W':
             visited = [[False] * m for _ in range(n)]
             graph[i][j] = 0
             visited[i][j] = True
-            c = max(c, bfs(i, j))
-print(c)
+            ans = max(ans, bfs(i, j))
+
+print(ans)
+
--- 5430.py
+++ 5430.py
@@ -9,41 +9,37 @@ for _ in range(t):
     n = int(input())
     nums = input().rstrip()
     nums = nums[1:-1]
-    print(nums)
     nums = nums.split(',')
-    print(nums)
     nums = deque(nums)
-
-    print(nums)
 
     # print(len(nums))
     # deque는 빈배열도 1이었음,,,,,,,,
     # 대박.
 
-    # flag = 0
-    # even = 0
+    flag = 0
+    even = 0
 
-    # if n == 0:
-    #     nums = deque()
+    if n == 0:
+        nums = deque()
 
-    # for i in s:
-    #     if i == 'R':
-    #         even += 1
-    #     elif i == 'D':
-    #         if len(nums) == 0:
-    #             flag = 1
-    #             print('error')
-    #             break
-    #         else:
-    #             if even % 2 == 0:
-    #                 nums.popleft()
-    #             else:
-    #                 nums.pop()
+    for i in s:
+        if i == 'R':
+            even += 1
+        elif i == 'D':
+            if len(nums) == 0:
+                flag = 1
+                print('error')
+                break
+            else:
+                if even % 2 == 0:
+                    nums.popleft()
+                else:
+                    nums.pop()
     
-    # if flag == 0:
-    #     if even % 2 == 0:
-    #         print('[' + ','.join(nums) + ']')
-    #     else:
-    #         nums = list(nums)
-    #         nums = nums[::-1]
-    #         print('[' + ','.join(nums) + ']')
+    if flag == 0:
+        if even % 2 == 0:
+            print('[' + ','.join(nums) + ']')
+        else:
+            nums = list(nums)
+            nums = nums[::-1]
+            print('[' + ','.join(nums) + ']')
