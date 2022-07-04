91d7b91c9569eb59282775b13e3bb65d3578c252
cokkum113 <codingwhizkid@naver.com>
2022-07-04 14:43:51 +0900

    1학년_bt

--- 5557.py
+++ 5557.py
@@ -1,29 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10000)
 
 n = int(input())
 nums = list(map(int, input().split()))
 
-answer = nums[-1]
-xx = nums[:-1]
+ans = nums[-1]
+numbers = nums[:-1]
 cnt = 0
 def backtracking(index, total):
     global cnt
-    if index == len(xx):
-        if total == answer:
+    if index == n - 1:
+        if total == ans:
             cnt += 1
+        return
     else:
-        sum1 = total + xx[index]
-        sum2 = total - xx[index]
+        sum_plus = total + numbers[index]
+        sum_sub = total - numbers[index]
 
-        if 0 <= sum1 <= 20:
-            backtracking(index + 1, sum1)
-        if 0 <= sum2 <= 20:
-            backtracking(index + 1, sum2)
+        if 0 <= sum_plus <= 20:
+            backtracking(index + 1, sum_plus)
+        if 0 <= sum_sub <= 20:
+            backtracking(index + 1, sum_sub)
 
 backtracking(0, 0)
 print(cnt)
-
-# 백트래킹에서 시간초과가 나면
-# dp로 답을 저장해나가는 방식으로 풀면 될거 같은데 ,,,,,,,
