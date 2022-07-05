e0c08804ba9d17248481b4db21b91020a8e0c41a
cokkum113 <codingwhizkid@naver.com>
2022-07-05 18:45:18 +0900

    heap

--- 11000.py
+++ 11000.py
@@ -1,23 +1,24 @@
+from curses import REPORT_MOUSE_POSITION
 import sys
 input = sys.stdin.readline
 import heapq
 
-time = []
 n = int(input())
+room = []
+
 for _ in range(n):
-    time.append(list(map(int, input().split())))
+    room.append(list(map(int, input().split())))
 
-
-time.sort(key = lambda x : (x[0], x[1]))
+room.sort(key=lambda x : x[0])
 
 heap = []
-heapq.heappush(heap, time[0][1])
+heapq.heappush(heap, room[0][1])
 
 for i in range(1, n):
-    if len(heap) != 0 and time[i][0] < heap[0]:
-        heapq.heappush(heap, time[i][1])
+    if len(heap) != 0 and room[i][0] < heap[0]:
+        heapq.heappush(heap, room[i][1])
     elif len(heap) != 0:
         heapq.heappop(heap)
-        heapq.heappush(heap, time[i][1])
-
+        heapq.heappush(heap, room[i][1])
+    
 print(len(heap))
