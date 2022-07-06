1c2a4c66993873ebbfb35d7850147c5ba638682c
cokkum113 <codingwhizkid@naver.com>
2022-07-06 18:43:16 +0900

    bactracking_시간초과

--- 1038_back.py
+++ 1038_back.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
 cnt = 9
-def backtracking(depth, limit, temp):
+n = int(input())
+
+def backtracking(index, limit, temp):
     global cnt
 
-    if depth == limit:
+    if index == limit:
         cnt += 1
-        
+
         if cnt == n:
             print(temp)
             exit()
-        
+    
     for i in range(int(temp[-1])):
-        backtracking(depth + 1, limit, temp + str(i))
+        backtracking(index + 1, limit, temp + str(i))
+    
 
 if n < 10:
     print(n)
     exit()
 else:
-    for k in range(1, 11):
-        for kk in range(1, 10):
-            backtracking(0, k, str(kk))
+    for i in range(1, 11):
+        for j in range(1, 10):
+            backtracking(0, i, str(j))
 
 print(-1)
+    
+
--- 5557.py
+++ 5557.py
@@ -1,27 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10**5)
+
 
 n = int(input())
 nums = list(map(int, input().split()))
 
 ans = nums[-1]
-numbers = nums[:-1]
+nums = nums[:-1]
 cnt = 0
 def backtracking(index, total):
-    global cnt
+    global cnt 
     if index == n - 1:
         if total == ans:
             cnt += 1
-        return
+            return
     else:
-        sum_plus = total + numbers[index]
-        sum_sub = total - numbers[index]
-
-        if 0 <= sum_plus <= 20:
-            backtracking(index + 1, sum_plus)
-        if 0 <= sum_sub <= 20:
-            backtracking(index + 1, sum_sub)
+        sum1 = total + nums[index]
+        sum2 = total - nums[index]
+        if 0 <= sum1 <= 20:
+            backtracking(index + 1, sum1)
+        if 0 <= sum2 <= 20:
+            backtracking(index + 1, sum2)
 
 backtracking(0, 0)
 print(cnt)
