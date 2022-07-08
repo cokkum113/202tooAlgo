4701ce04a5738df54972928c7ce3550005247108
cokkum113 <codingwhizkid@naver.com>
2022-07-08 18:20:42 +0900

    3차원 bfs

--- 6593.py
+++ 6593.py
@@ -1,6 +1,6 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 dx = [0, 0, 1, -1, 0, 0]
 dy = [0, 0, 0, 0, 1, -1]
@@ -13,45 +13,42 @@ def bfs():
 
     while que:
         zz, xx, yy = que.popleft()
-
         for d in range(6):
-            nz = zz + dz[d]
-            nx = xx + dx[d]
-            ny = yy + dy[d]
+            nz = dz[d] + zz
+            nx = dx[d] + xx
+            ny = dy[d] + yy
 
             if 0 <= nz < l and 0 <= nx < r and 0 <= ny < c:
                 if visited[nz][nx][ny] == False:
-                    if graph[nz][nx][ny] == '.' or graph[nz][nx][ny] == 'E':
-                        cnt[nz][nx][ny] = cnt[zz][xx][yy] + 1
+                    if graph[nz][nx][ny] != '#':
                         visited[nz][nx][ny] = True
+                        cnt[nz][nx][ny] = cnt[zz][xx][yy] + 1
                         que.append([nz, nx, ny])
 
-while(True):
+while True:
     l, r, c = map(int, input().split())
     if l == 0 and r == 0 and c == 0:
         break
     graph = [[] * r for _ in range(l)]
-    cnt = [[[0] * c for _ in range(r)]for _ in range(l)]
-    visited = [[[False]* c for _ in range(r)]for _ in range(l)]
+    cnt = [[[0] * c for _ in range(r)] for _ in range(l)]
+    visited = [[[False] * c for _ in range(r)] for _ in range(l)]
+
+
     for i in range(l):
         for j in range(r):
             graph[i].append(list(input().rstrip()))
-        
         input()
 
     for i in range(l):
         for j in range(r):
             for k in range(c):
                 if graph[i][j][k] == 'S':
-                    sz = i
-                    sx = j
-                    sy = k
+                    sz, sx, sy = i, j, k
                 elif graph[i][j][k] == 'E':
-                    ez = i
-                    ex = j
-                    ey = k
+                    ez, ex, ey = i, j, k
     bfs()
     if cnt[ez][ex][ey] != 0:
         print("Escaped in %d minute(s)." %cnt[ez][ex][ey])
     else:
         print("Trapped!")
+
