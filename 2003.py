77815559c3385d8926fbe8cc2f60228493b7e4eb
cokkum113 <codingwhizkid@naver.com>
2022-07-17 17:41:56 +0900

    2003

--- 2003.py
+++ 2003.py
@@ -8,26 +8,28 @@ nums = list(map(int, input().split()))
 lo = 0
 hi = 0
 
-ans = nums[0]
-
+total = nums[hi]
 cnt = 0
+
 while True:
-    if hi == n - 1 and ans < m:
+    if hi == n - 1 and lo > hi:
         break
 
-    if ans < m:
-        hi += 1
-        ans += nums[hi]
-    
-    elif ans > m:
-        ans -= nums[lo]
-        lo += 1
-    
-    elif ans == m:
-        ans -= nums[lo]
+    if total == m:
         cnt += 1
+        total -= nums[lo]
         lo += 1
+        
+    elif total > m:
+        total -= nums[lo]
+        lo += 1
+        
+    elif total < m:
+        if hi == n - 1:
+            total -= nums[lo]
+            lo += 1
+        elif hi < n - 1:
+            hi += 1
+            total += nums[hi]
 
 print(cnt)
-    
-
