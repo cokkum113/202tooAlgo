965865988dd29bad4c255531ba011f3cea6c271d
cokkum113 <codingwhizkid@naver.com>
2022-07-18 18:03:21 +0900

    11053

--- 11053.py
+++ 11053.py
@@ -1,14 +1,20 @@
+from random import randrange
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
 
+# ans = [1] * n
+# 다이나믹프로그램으로 푸는 문제
 dp = [1] * n
+# 현재 위치까지 가장 증가된 큰값들 넣기
 
-for i in range(1, n):
+# 1, 4, 2, 3, 5
+#cnt같은 값이 필요없음
+for i in range(n):
     for j in range(i):
-        if nums[j] < nums[i]:
-            dp[i]  = max(dp[i], dp[j] + 1)
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
