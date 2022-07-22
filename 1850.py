560fc8cdba42c927e6a0f3bfd74e7cc36376aa17
cokkum113 <codingwhizkid@naver.com>
2022-07-22 16:20:48 +0900

    1850

--- 1850.py
+++ 1850.py
@@ -6,17 +6,13 @@ ss.sort()
 a = ss[0]
 b = ss[1]
 
-ca = int('1' * a)
-cb = int('1' * b)
+# ca = int('1' * a)
+# cb = int('1' * b)
 
 def gcd(x, y):
     while y > 0:
         x, y = y, x%y
     return x
 
-print(gcd(ca, cb))
+print('1' * gcd(a, b))
 
-# while cb > 0:
-#     ca, cb = cb, ca%cb
-
-# print(ca)
