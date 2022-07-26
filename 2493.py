96a34274418c8c7aefff7e927ec1d294571a3af3
cokkum113 <codingwhizkid@naver.com>
2022-07-26 15:46:45 +0900

    stack

--- 2493.py
+++ 2493.py
@@ -1,42 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+# 현재시점에서만 상관있는걸 보면되는 상태?
 n = int(input())
 top = list(map(int, input().split()))
 
-ans = [0] * n
-# 뒤에 더 큰게 나타나면 쓸모가 없어짐
 stack = []
-
-# def check(stack, now_pos):
-#     for i in range(len(stack)-1, -1, -1):
-#         if now_pos < stack[i][1]:
-#             return stack[i][0]
-
+ans = [0] * n
 for i in range(n):
+    # 나보다 작은 거 빼기
+    while stack and stack[-1][1] < top[i]:
+        stack.pop()
+    
     if not stack:
         ans[i] = 0
-    # else:
-    #     if stack[-1][1] >= top[i]:
-    #         ans[i] = stack[-1][0]
-    #     else:
-            # 이 부분 처리가 결국 시간초과나는 코드 for문안에서 결국
-            # for문을 돌게됨
+    else:
+        ans[i] = stack[-1][0]
+    
 
-            # if check(stack, top[i]):
-            #     ans[i] = check(stack, top[i])
-            # else:
-            #     ans[i] = 0
-            # stack.pop()
-    # 스택안에서만 돌게 하면됨
+    stack.append([i + 1,top[i]])
 
-    while stack:
-        if stack[-1][1] >= top[i]:
-            ans[i] = stack[-1][0]
-            break
-        else:
-            stack.pop()
-    stack.append([i + 1, top[i]])
-
-
-print(' '.join(map(str,(ans))))
+print(' '.join(map(str,ans)))
