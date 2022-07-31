8c52b467021a0bbae7fc980456662b4060c9bda8
cokkum113 <codingwhizkid@naver.com>
2022-08-01 00:38:36 +0900

    9251

--- 9251.py
+++ 9251.py
@@ -1,29 +1,21 @@
-# dp문제는 인덱스를 뭐를 잡고 갈 것인지가 중요
 import sys
 input = sys.stdin.readline
 
-a = input().strip()
-b = input().strip()
-# 가장 긴 숫자까지 어떻게 길게 왔는지 계속더해가는 식으로 풀면 될듯하다
+s1 = input().rstrip()
+s2 = input().rstrip()
 
-# a를 크게 돌면서 b를 돌면서 어느 길이에서 어떻게 끊기는 지 보면될거같다
+s1 = ' ' + s1
+s2 = ' ' + s2
 
-a = ' ' + a
-b = ' ' + b
+# i번째의 위치에 있을 때, j번째 수까지 제일 최장 거리를 구하기
 
-dp = [[0] * len(b) for _ in range(len(a))]
+dp = [[0] * (len(s2)) for _ in range(len(s1))]
 
-
-# print(a, b)
-
-# 문자열1을 하나를 잡고 돌 때
-# 문자열2에 있는 값이 맞으면 그 전꺼중에 제일 큰 값과 더해주기
-for i in range(1, len(a)):
-    for j in range(1, len(b)):
-        if a[i] == b[j]:
+for i in range(1, len(s1)):
+    for j in range(1, len(s2)):
+        if s1[i] == s2[j]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-            
-#dp[-1]이 4가 나와야지 정상
+
 print(max(dp[-1]))
