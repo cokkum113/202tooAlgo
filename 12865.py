24f9261df4afda13317784a22e6644cdbb27964e
cokkum113 <codingwhizkid@naver.com>
2022-07-31 23:56:22 +0900

    배낭문제

--- 12865.py
+++ 12865.py
@@ -14,7 +14,12 @@ dp = [[0] * (k + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, k + 1):
-        if ll[i][0] < j:
+        if ll[i][0] <= j:
+            # 같을때랑 작을때랑 처리가 같음
             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - ll[i][0]] + ll[i][1])
+        else:
+            # 클때 처리도 잘 해줘야함
+            dp[i][j] = dp[i - 1][j]
 
 print(dp)
+print(max(dp[-1]))
