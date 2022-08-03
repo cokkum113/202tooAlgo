3a208b20e0b60be08fc4fabecd92abb2556e1b32
cokkum113 <codingwhizkid@naver.com>
2022-08-03 18:23:33 +0900

    6603

--- 6603.py
+++ 6603.py
@@ -8,21 +8,23 @@ def backtracking(index):
     
     for i in range(n):
         if visited[i] == False:
-            nums.append(xx[i])
-            visited[i] = True
-            backtracking(index + 1)
-            visited[i] = False
-            nums.pop()
+            if nums and nums[-1] < xx[i] or len(nums) == 0:
+                nums.append(xx[i])
+                visited[i] = True
+                backtracking(index + 1)
+                nums.pop()
+                visited[i] = False
 
 while True:
     x = list(map(int, input().split()))
     n = x[0]
     nums = []
-
-    visited = [False] * n
     if n == 0:
         break
+
     else:
         xx = x[1:]
+        visited = [False] * n
         backtracking(0)
-
+    
+    print()
