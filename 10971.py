cc6926b3cfc47b2dd5f96f5c47678357a679fbdf
cokkum113 <codingwhizkid@naver.com>
2022-08-05 17:55:36 +0900

    10971

--- 10971.py
+++ 10971.py
@@ -1,36 +1,41 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 
+visited = [False] * n
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-    
-visited = [False] * n
 
-# 문제를 정의해보자 : n까지 다 돌아서의 mini값을 찾은다음에 mini값을 출력
 mini = int(1e9)
 total = 0
-def backtracking(depth, r, c):
+
+# 시작점, 끝점
+# 문제 정의 : n까지 다 돌아서 mini값을 찾은 다음에 mini값을 출력
+def backtracking(depth, st, v):
     global mini
     global total
-    if depth == n and graph[r][c] != 0:
-        mini = min(mini, total + graph[r][c])
-        return
+    
+    if depth == n and graph[st][v] != 0:
+        #st에서 v로 오는 길
+        mini = min(total + graph[st][v], mini)
     
     for i in range(n):
-        if visited[i] == False and graph[r][i] != 0:
+        if visited[i] == False and graph[st][i] != 0:
+            total += graph[st][i]
             visited[i] = True
-            total += graph[r][i]
-            backtracking(depth + 1, i, c)
-            total -= graph[r][i]
+            backtracking(depth + 1, i, v)
+            # depth + 1은 OK
+            # i # 시작점이 옮겨간거 OK
+            # v는 끝점이니까 두기
             visited[i] = False
-
+            total -= graph[st][i]
 
 for i in range(n):
     visited[i] = True
     backtracking(1, i, i)
-    ##################r, c
-    visited[i] = False   
+    visited[i] = False
 
 print(mini)
+
