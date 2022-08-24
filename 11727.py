353fd48cf2e3c18ca0f0b495f112fdb6c8085aab
cokkum113 <codingwhizkid@naver.com>
2022-08-24 23:40:24 +0900

    11727

--- 11727.py
+++ 11727.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [0] * (n + 1)
+dp = [0] * (10001)
 dp[0] = 0
 dp[1] = 1
 dp[2] = 3
 for i in range(3, n + 1):
-    dp[i] = dp[i - 2] * 2 + dp[i - 1]
+    dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007
 
 print(dp[n])
