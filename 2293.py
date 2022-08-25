bce7b0003dae05fe76a6a5a8d95f920114041513
cokkum113 <codingwhizkid@naver.com>
2022-08-25 16:13:42 +0900

    2293

---
+++ 2293.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for i in nums:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            dp[j] += dp[j - i]
+
+print(dp[k])
