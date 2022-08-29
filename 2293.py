f2363c6265fe35647dbed38f4bdd4611d8b3a26c
cokkum113 <codingwhizkid@naver.com>
2022-08-29 17:36:16 +0900

    2293

--- 2293.py
+++ 2293.py
@@ -8,11 +8,11 @@ for _ in range(n):
     nums.append(int(input()))
 
 dp = [0] * (k + 1)
+
 dp[0] = 1
 
 for i in nums:
     for j in range(1, k + 1):
         if j - i >= 0:
             dp[j] += dp[j - i]
-
-print(dp[k])
+print(dp[-1])
