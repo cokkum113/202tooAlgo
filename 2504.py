4a2c114c65381b8bbe86f64266a6f660a9445196
cokkum113 <codingwhizkid@naver.com>
2022-09-08 01:41:23 +0900

    2504

---
+++ 2504.py
@@ -0,0 +1,58 @@
+import sys
+input = sys.stdin.readline
+
+ss = input().rstrip()
+st = []
+ch = []
+
+for i in ss:
+    if i == '(' or i == '[':
+        ch.append(i)
+    elif i == ']':
+        if ch and ch[-1] == '[':
+            ch.pop()
+    elif i == ')':
+        if ch and ch[-1] == '(':
+            ch.pop()
+
+if len(ch) != 0:
+    print(0)
+    exit()
+
+for i in ss:
+    if i == '(' or i == '[':
+        st.append(i)
+    
+    elif i == ')':
+        if st:
+            if str(st[-1]).isdigit():
+                total = 0
+                for j in range(len(st) - 1, - 1, - 1):
+                    if st[j] == '(':
+                        st[j] = total * 2
+                        break
+                    else:
+                        total += st[j]
+                        st.pop()
+            else:
+                st.pop()
+                st.append(2)
+
+    elif i == ']':
+        if st:
+            if str(st[-1]).isdigit():
+                total = 0
+                for j in range(len(st) - 1, - 1, - 1):
+                    if st[j] == '[':
+                        st[j] = total * 3
+                        break
+                    else:
+                        total += st[j]
+                        st.pop()
+            else:
+                st.pop()
+                st.append(3)
+
+print(sum(st))
+                
+            
