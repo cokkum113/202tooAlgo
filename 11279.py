372a33aa21a0f2fdcbd0e3cd074bb6a50eafdf85
cokkum113 <codingwhizkid@naver.com>
2022-09-10 19:17:53 +0900

    최대힙구현

--- 11279.py
+++ 11279.py
@@ -4,45 +4,53 @@ n = int(input())
 
 heap = [-1] * 100001
 heap_size = 0
+
 def push(x):
-    global heap
     global heap_size
     heap_size += 1
     heap[heap_size] = x
-    parent = heap_size//2
+
+    parent = heap_size // 2
     child = heap_size
+
+    while (child > 1) and (heap[parent] < heap[child]):
+        heap[parent], heap[child] = heap[child], heap[parent]
+        parent, child = child, parent
     
-    while(child > 1) and (heap[child] > heap[parent]):
-        heap[child], heap[parent] = heap[parent], heap[child]
-        child = parent
-        parent = child
+
 def pop():
-    global heap
     global heap_size
-    result = heap[1]
+    pop_val = heap[1]
+
     heap[1] = heap[heap_size]
     heap[heap_size] = 0
+
     heap_size -= 1
+
     parent = 1
     child = 2
-    #do-while
-    if (child+1 <= heap_size) and (heap[child] < heap[child+1]):
+
+    if (child + 1 <= heap_size) and (heap[child] < heap[child + 1]):
         child = child + 1
+    
     while (child <= heap_size) and (heap[parent] < heap[child]):
-        heap[parent], heap[child] = heap[child], heap[parent]
+        heap[child], heap[parent] = heap[parent], heap[child]
         parent = child
-        child = parent*2
-        if (child+1 <= heap_size) and (heap[child] < heap[child+1]):
+        child = parent * 2
+
+        if (child + 1 <= heap_size) and heap[child] < heap[child + 1]:
             child = child + 1
-    return result
+    
+    return pop_val
+
 
 for i in range(n):
-    node = int(input())
-    if node == 0: 
+    x = int(input())
+    if x == 0:
         if heap_size > 0:
-            ans = pop()
-            print(ans)
+            print(pop())
         else:
             print(0)
+    
     else:
-        push(node)
+        push(x)
