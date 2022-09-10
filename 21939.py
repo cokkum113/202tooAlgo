21c61daf238cd522ae875187845e579f01d03aa4
cokkum113 <codingwhizkid@naver.com>
2022-09-10 12:30:16 +0900

    cleanCode:

--- 21939.py
+++ 21939.py
@@ -4,9 +4,7 @@ input = sys.stdin.readline
 
 import heapq
 
-# 문제가 쉬운 순
 miniheap = []
-# 문제가 어려운 순
 maxiheap = []
 
 n = int(input())
@@ -15,55 +13,45 @@ for _ in range(n):
     x, y = map(int, input().split())
     heapq.heappush(maxiheap, [-y, -x])
     heapq.heappush(miniheap, [y, x])
-    diction[x].append(y)
-    # 큐에 만약 문제번호 7과 100, 7과 300이 있으면 마지막 들어온게 난이도고, 그 난이도가 maxiheap에 있다면 그건 유효한 값
+    diction[x] = y
 
+def recommend(heap):
+    while heap:
+        xx = 0
+        if not heap:
+            break
+        if heap[0][1] < 0:
+            xx = - heap[0][1]
+        else:
+            xx = heap[0][1]
+        
+        if diction[xx]:
+            now_validation = diction[xx]
+            if now_validation == -heap[0][0] or now_validation == heap[0][0]:
+                return xx
+            else:
+                if diction[xx]:
+                    diction[xx] = 0
+        heapq.heappop(heap)
+        
+
+        
 m = int(input())
 for _ in range(m):
     ss = input().rstrip().split()
     # 파라미터로  heap만 넣게 하기, 함수로 빼기
     if ss[0] == "recommend":
         if ss[1] == "1":
-            if maxiheap:
-                while maxiheap:
-                    if not maxiheap:
-                        break
-                    xx = -maxiheap[0][1]
-                    if diction[xx]:
-                        now_dict = diction[xx]
-                        now_validation = now_dict[-1]
-                        if now_validation == -maxiheap[0][0]:
-                            print(xx)
-                            break
-                        else:
-                            if diction[xx]:
-                                now_dict = diction[xx]
-                                now_dict.pop()
-                    heapq.heappop(maxiheap)
+            print(recommend(maxiheap))
 
         elif ss[1] == "-1":
-            if miniheap:
-                while miniheap:
-                    if not miniheap:
-                        break
-                    xx = miniheap[0][1]
-                    if diction[xx]:
-                        now_dict = diction[xx]
-                        now_validation = now_dict[-1]
-                        if now_validation == miniheap[0][0]:
-                            print(xx)
-                            break
-                        else:
-                            if diction[xx]:
-                                now_dict = diction[xx]
-                                now_dict.pop()
-                    heapq.heappop(miniheap)
+            print(recommend(miniheap))
 
     elif ss[0] == "add":
         heapq.heappush(maxiheap, [-int(ss[2]), -int(ss[1])])
         heapq.heappush(miniheap, [int(ss[2]), int(ss[1])])
-        diction[int(ss[1])].append(int(ss[2]))
+        diction[int(ss[1])]= int(ss[2])
 
     else:
         if diction[int(ss[1])]:
-            diction[int(ss[1])].pop()
+            diction[int(ss[1])] = 0
