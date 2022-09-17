3ada6891e848f55a7f8dbdbd7d72b98009d8a74d
cokkum113 <codingwhizkid@naver.com>
2022-09-17 13:35:39 +0900

    17073

--- 17073.py
+++ 17073.py
@@ -2,25 +2,36 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-# 하나 주고 -> 쌓아두고 -> 이것을 같이 하지 않을 것
-
-# 부모노드에 물이 없을 때를 구하면 됨. ->
-# 제일 리프노드랑 관련되어 있는 거같은데
-# 루트노드가 아닌 리프노드 수로 20을 나누면?
-
-
 u, v = map(int, input().split())
 
+leaf = [[] for _ in range(u + 1)]
+
 tree = [[] for _ in range(u + 1)]
+
+visited = [False] * (u + 1)
+
+def bfs(x):
+    que = deque()
+    que.append(x)
+    visited[x] = True
+
+    while que:
+        xx = que.popleft()
+        for nx in tree[xx]:
+            if not visited[nx]:
+                que.append(nx)
+                visited[nx] = True
+                leaf[xx].append(nx)
 
 for _ in range(u - 1):
     x, y = map(int, input().split())
     tree[x].append(y)
     tree[y].append(x)
+bfs(1)
 
 cnt = 0
-for i in range(1, len(tree)):
-    if len(tree[i]) == 0:
+for i in range(1, len(leaf)):
+    if len(leaf[i]) == 0:
         cnt += 1
 
 print(v/cnt)
