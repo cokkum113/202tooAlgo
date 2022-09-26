c90a1321c3947d9acfdb329d6ab89bc1370f2217
cokkum113 <codingwhizkid@naver.com>
2022-09-26 13:48:43 +0900

    bj

--- 17609.py
+++ 17609.py
@@ -1,32 +1,67 @@
 import sys
 input = sys.stdin.readline
 
+# 회문 0
+# 유사회문 1
+# 그외 2
+
+
+def p(s):
+    for i in range(1, len(s)//2 + 1):
+        if s[i - 1] != s[-i]:
+            return False
+    return True
+
+def p2(s):
+    flag = False
+    lo = 0
+    hi = len(s) - 1
+    while lo < hi:
+        if not flag and s[lo] == s[hi]:
+            lo += 1
+            hi -= 1
+        elif not flag and s[lo] != s[hi]:
+            flag = True
+            lo += 1
+        
+        elif flag and s[lo] == s[hi]:
+            lo += 1
+            hi -= 1
+        
+        elif flag and s[lo] != s[hi]:
+            return False
+    return True
+        
+def p22(s):
+    flag = False
+    lo = 0
+    hi = len(s) - 1
+    while lo < hi:
+        if not flag and s[lo] == s[hi]:
+            lo += 1
+            hi -= 1
+        elif not flag and s[lo] != s[hi]:
+            flag = True
+            hi -= 1
+        
+        elif flag and s[lo] == s[hi]:
+            lo += 1
+            hi -= 1
+        
+        elif flag and s[lo] != s[hi]:
+            return False
+    return True
+        
+
 t = int(input())
-
-def one(s, st, end):
-    while st < end:
-        if s[st] == s[end]:
-            st += 1
-            end -= 1
-        else:
-            return 2
-    return 1
-
-def zero(s, st, end):
-    while st < end:
-        if s[st] == s[end]:
-            st += 1
-            end -= 1
-        else:
-            new_s1 = one(s, st + 1, end)
-            new_s2 = one(s, st, end -1)
-            if (new_s1 == 1 and new_s2 == 2) or (new_s1 == 2 and new_s2 == 1) or (new_s1 == 1 and new_s2 == 1):
-                return 1
-            else:
-                return 2
-    return 0
-
-
 for _ in range(t):
     s = input().rstrip()
-    print(zero(s, 0, len(s) - 1))
+    if p(s):
+        print(0)
+    else:
+        if p2(s) or p22(s):
+            print(1)
+        else:
+            print(2)
+
+
