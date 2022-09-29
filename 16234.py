c6d17ef7fbf3811f918854432ec4402e68e58d96
cokkum113 <codingwhizkid@naver.com>
2022-09-29 18:02:12 +0900

    16324

--- 16234.py
+++ 16234.py
@@ -1,59 +1,60 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
+
 
 n, l, r = map(int, input().split())
-
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+visited = [[False] * n for _ in range(n)]
 
-def bfs(sx, sy):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+movelist = []
+
+def bfs(x, y):
     global flag
     que = deque()
-    que.append([sx, sy])
-    visited[sx][sy] = True
+    que.append([x, y])
+    visited[x][y] = True
 
-    total = 0
-    # total = graph[sx][sy] 가 아니어도됨 아래서 처리를 해주니까
-    size = 1
-    # 한칸한칸일때의 사이즈
+    unit_cnt = 1
+    unit_people = 0
 
-    golist = []
-    # 현재 유닛이 될 수 있는 리스트를 담을 곳이 필요함
-    golist.append([sx, sy])
+    movelist = []
+
+    movelist.append([x, y])
 
     while que:
         xx, yy = que.popleft()
-        total += graph[xx][yy]
-
+        unit_people += graph[xx][yy]
         for d in range(4):
             nx = dx[d] + xx
             ny = dy[d] + yy
 
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False:
-                if l <= abs(graph[nx][ny] - graph[xx][yy]) <= r:
-                    size += 1
-                    golist.append([nx, ny])
-                    que.append([nx, ny])
-                    visited[nx][ny] = True
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False and l <=  abs(graph[nx][ny] - graph[xx][yy]) <= r:
+                visited[nx][ny] = True
+                unit_cnt += 1
+                movelist.append([nx, ny])
+                que.append([nx, ny])
     
-    if size > 1:
-        flag = True
-        # 연합된 상태
-        new_people_cnt = total // size
-        for i in golist:
-            graph[i[0]][i[1]] = new_people_cnt
-                    
-cnt = 0
+    if unit_cnt > 1:
+        for xxx, yyy in movelist:
+            graph[xxx][yyy] = unit_people//unit_cnt
+            flag = True
+    # else:
+    #     while movelist:
+    #         movelist.pop()
 
+
+cnt = 0
 while True:
-    flag = False
-    # 동시이동을 위한 것
+    # print(graph)
     visited = [[False] * n for _ in range(n)]
+    flag = False
     for i in range(n):
         for j in range(n):
             if visited[i][j] == False:
@@ -61,7 +62,9 @@ while True:
     
     if flag:
         cnt += 1
+    
     else:
         break
 
 print(cnt)
+
--- 9996.py
+++ 9996.py
@@ -1,20 +1,29 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
+# n = int(input())
+# pattern = input().rstrip()
+
+# p = pattern.split('*')
+# pp = ''.join(p)
+# for _ in range(n):
+#     s = input().rstrip()
+#     s1 = s[:len(p[0])]
+#     s2 = s[-len(p[1]):]
+
+#     if s1 == p[0] and s2 == p[1] and len(s) >= len(pp):
+#         print("DA")
+#     else:
+#         print("NE")
+
+
+# import re
+# i=input
+# T,p=int(i()),i().replace("*",".*")+"$"
+# exec('print("DA"if re.match(p,i())else"NE");'*T)
+
+import re
 n = int(input())
-pattern = input().rstrip()
+p = input().replace("*", ".*")+"$"
+exec('print("DA" if re.match(p, input().rstrip())else "NE");'*n)
 
-p = pattern.split('*')
-
-for _ in range(n):
-    s = input().rstrip()
-    s1 = s[:len(p[0])]
-    s2 = s[-len(p[1]):]
-
-    if len(s) < len(p):
-        print("NE")
-    else:
-        if s1 == p[0] and s2 == p[1] and len(s) >= len(p):
-            print("DA")
-        else:
-            print("NE")
